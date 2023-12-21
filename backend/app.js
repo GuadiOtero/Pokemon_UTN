f660d1f608a190fb3dfe5f337a1317088a645299
GUADALUPE OTERO <GOTERO@galiciaseguros.com.ar>
2023-12-21 15:16:49 -0300

    se agregan funcionalidades con express-session en el back

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var novedadesRouter = require('./routes/novedades');
-var catalogoRouter = require('./routes/catalogo');
-var usuariosRouter = require('./routes/usuarios');
+// var loginRouter = require('./routes/admin/login');
+
 
 var app = express();
 
@@ -22,11 +22,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/novedades', novedadesRouter);
-app.use('/catalogo', catalogoRouter);
-app.use('/usuarios', usuariosRouter);
+// Sesiones
+app.use(session({
+  secret: '7qe22@gh400!',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/admin/login', loginRouter);
+
+const registeredUsers = ['guadalupe', 'guadalupe01', 'guadalupe02'];
+
+app.get('/', function(req,res){
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'Iniciar Sesión PokeGame',
+    mensaje: 'Bienvenido',
+    conocido: conocido,
+    nombre: req.session.nombre
+  })
+});
+
+app.post('/ingresar', function(req, res) {
+  const userIngresado = req.body.nombre;
+
+  // Verifico si el usuario esta registrado
+
+  if (registeredUsers.includes(userIngresado)) {
+    req.session.nombre = userIngresado;
+    res.redirect('/');
+  } else {
+    res.render('index', {
+      title: 'Iniciar Sesión PokeGame',
+      conocido: false,
+      nombre: null,
+      mensaje: 'Usuario no registrado',
+    });
+  }
+});
+
+app.get('/salir', function(req,res){
+  req.session.destroy();
+  res.redirect('/');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
